05b3eff355dd08d38d9262d3ba9f8e48b899358d
panpan <panpan31415@gmail.com>
2019-10-24 14:39:02 +0200

    build aain

---
+++ docs/build/precache-manifest.27048c266d909c6486b034bbad6ee8de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e444fcad16ffbee6f348c6c3c6443e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0e64bbbabac7aa3a526",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "710536b1265c3db3fbd1",
+    "url": "/static/css/main.c1f03d71.chunk.css"
+  },
+  {
+    "revision": "d0e64bbbabac7aa3a526",
+    "url": "/static/js/2.59341cbb.chunk.js"
+  },
+  {
+    "revision": "710536b1265c3db3fbd1",
+    "url": "/static/js/main.e828f94f.chunk.js"
+  },
+  {
+    "revision": "049c0550b396f3cb057e",
+    "url": "/static/js/runtime-main.9bb50536.js"
+  },
+  {
+    "revision": "26b387d74d6f7589153f1a1d4931ccff",
+    "url": "/static/media/logo.26b387d7.png"
+  }
+]);
