cbbdd8470cac2e890c4cf8d0e4ff4706553a5763
panpan <panpan31415@gmail.com>
2019-10-24 13:42:14 +0200

    add event website component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.png"
+import "./App.css"
+import ReactTable from "react-table"
+import "react-table/react-table.css"
+import motosumo from "./api/motosumo"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    id: 42,
+    event: []
+  }
+
+  fetchEvent = async id => {
+    console.log("fetching event..")
+    const response = await motosumo.get("/event", {
+      params: {
+        id: id
+      }
+    })
+    console.log(response)
+    this.setState({
+      event: response.data
+    })
+  }
+
+  componentDidMount() {
+    // fetch event now, and then every 1 minute
+    this.fetchEvent(this.id)
+    this.timer = setInterval(() => this.fetchEvent(), 60000)
+  }
+
+  componentDidUpdate() {
+    console.log("UPDATE: ", this.state.event)
+  }
+
+  render() {
+    const columns = [
+      {
+        Header: "Chain",
+        accessor: "chain"
+      },
+      {
+        Header: "Gym",
+        accessor: "center"
+      },
+      {
+        Header: "Distance",
+        accessor: "distance"
+      },
+      {
+        Header: "kcal",
+        accessor: "kcal"
+      },
+      {
+        Header: "Hours",
+        accessor: "hours"
+      },
+      {
+        Header: "Unique members",
+        accessor: "n_users"
+      }
+    ]
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to the Motosumo Global Event site!</p>
+          <div className="App-table">
+            <ReactTable data={this.state.event} columns={columns} />
+          </div>
+          <p className="App-link">
+            Powered by&nbsp;
+            <a
+              className="App-link"
+              href="https://motosumo.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Motosumo
+            </a>
+          </p>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/api/motosumo.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+
+export default axios.create({
+  //   baseURL: "https://rest-dev.mo2tion.com/v2/api/event",
+  baseURL: "https://my-json-server.typicode.com/mo2tion/fake-event"
+})
