7419b5a8a7ad7887262d304c4e642ad9894fe344
panpan <panpan31415@gmail.com>
2019-10-24 14:12:25 +0200

    another build

---
+++ docs/precache-manifest.5bd8c1e5b2ef2a43fcfcc4558e01a17b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203db247c9298e13419ebc226643b468",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "d0e64bbbabac7aa3a526",
+    "url": "/react-table/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "ee3401f7a7ff29237cd5",
+    "url": "/react-table/static/css/main.c1f03d71.chunk.css"
+  },
+  {
+    "revision": "d0e64bbbabac7aa3a526",
+    "url": "/react-table/static/js/2.59341cbb.chunk.js"
+  },
+  {
+    "revision": "ee3401f7a7ff29237cd5",
+    "url": "/react-table/static/js/main.2e3ab5b2.chunk.js"
+  },
+  {
+    "revision": "99473146ab6c99a75099",
+    "url": "/react-table/static/js/runtime-main.648f6213.js"
+  },
+  {
+    "revision": "26b387d74d6f7589153f1a1d4931ccff",
+    "url": "/react-table/static/media/logo.26b387d7.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8e2f91b966c3ab623b61b3d561f9284b.js"
+  "/react-table/precache-manifest.5bd8c1e5b2ef2a43fcfcc4558e01a17b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-table/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
