e7adda81799ae7c49063a4e458f07d6cae7a569f
panpan <panpan31415@gmail.com>
2019-10-24 13:56:21 +0200

    second try

--- docs/api/motosumo.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  //   baseURL: "https://rest-dev.mo2tion.com/v2/api/event",
-  baseURL: "https://my-json-server.typicode.com/mo2tion/fake-event"
-});
---
+++ docs/precache-manifest.738854d37626eb54ff781644a3f7005e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56facac017193400f3bff5d390fbee4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ce4b342d1e967ff73e9",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "b8ca49b03c8dcba5c5a0",
+    "url": "/static/css/main.c1f03d71.chunk.css"
+  },
+  {
+    "revision": "5ce4b342d1e967ff73e9",
+    "url": "/static/js/2.37defb1d.chunk.js"
+  },
+  {
+    "revision": "b8ca49b03c8dcba5c5a0",
+    "url": "/static/js/main.467d449c.chunk.js"
+  },
+  {
+    "revision": "804fc2c69f783a8a1d20",
+    "url": "/static/js/runtime-main.ad6afe1b.js"
+  },
+  {
+    "revision": "26b387d74d6f7589153f1a1d4931ccff",
+    "url": "/static/media/logo.26b387d7.png"
+  }
+]);
